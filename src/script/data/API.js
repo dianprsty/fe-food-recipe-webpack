3f190135fca126e2a69150fcb192b71cb0aad45a
dianprsty <dprasmail@gmail.com>
2024-01-02 11:06:03 +0700

    feat: use local storage for caching

--- src/script/data/API.js
+++ src/script/data/API.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 import { hideLoading, showLoading } from "../utils/loading";
 
+const LOCAL_KEY = "foods_data";
+
 export const getAllRecipe = async () => {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   showLoading();
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const localData = localStorage.getItem(LOCAL_KEY);
+
+  if (localData) {
+    hideLoading();
+    return JSON.parse(localData);
+  }
+
   const data = await Promise.all(
     alphabet.map(async (char) => {
       let result = [];
@@ -12,9 +22,9 @@ export const getAllRecipe = async () => {
           `https://www.themealdb.com/api/json/v1/1/search.php?f=${char}`,
         );
       } catch (error) {
-        console.log(error);
         hideLoading();
       }
+
       return result.data.meals;
     }),
   );
@@ -25,18 +35,28 @@ export const getAllRecipe = async () => {
     if (meals) result.push(...meals);
   });
 
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(result));
+
   return result;
 };
 
 export const getRecipeById = async (id) => {
   let result = [];
+  const localData = localStorage.getItem(LOCAL_KEY);
+
+  if (localData) {
+    result = JSON.parse(localData).find((food) => {
+      return food.idMeal === id;
+    });
+    return result;
+  }
   try {
     showLoading();
     result = await axios.get(
       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
     );
   } catch (error) {
-    console.log(error);
+    //
   } finally {
     hideLoading();
   }
@@ -44,14 +64,22 @@ export const getRecipeById = async (id) => {
 };
 
 export const searchFoods = async (search) => {
+  const localData = localStorage.getItem(LOCAL_KEY);
+
   let result = [];
+  if (localData) {
+    result = JSON.parse(localData).filter((food) => {
+      return food.strMeal.toLowerCase().includes(search.toLowerCase());
+    });
+    return result;
+  }
   try {
     showLoading();
     result = await axios.get(
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`,
     );
   } catch (error) {
-    console.log(error);
+    //
   } finally {
     hideLoading();
   }
